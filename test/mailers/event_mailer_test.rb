fb79e4f9a00f3464504f9f324804a7a8a0e5d781
Elyasin Shaladi <Elyas.Shaladi@gmail.com>
2015-02-27 11:03:10 +0700

    Fix test cases event mailer.

--- test/mailers/event_mailer_test.rb
+++ test/mailers/event_mailer_test.rb
@@ -17,12 +17,12 @@ class EventMailerTest < ActionMailer::TestCase
 		email = EventMailer.item_created_message(@item1).deliver_now!
 		assert_not ActionMailer::Base.deliveries.empty?
 		assert_equal ['info@comemalaka.com'], email.from, "Sender email is not correct"
-		assert_includes email.to, 'Lasse Lund', "Recipient email is not correct"
-		assert_includes email.to, 'Elyasin Shaladi', "Recipient email is not correct"
-		assert_includes email.to, 'Theo Goumas', "Recipient email is not correct"
-		assert_includes email.to, 'Neal Mundy', "Recipient email is not correct"
-		assert_includes email.to, 'Juan Cabrera', "Recipient email is not correct"
-		assert_includes email.to, 'Nuno Fonseca', "Recipient email is not correct"
+		assert_includes email.to, 'organizer@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user1@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user2@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user3@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user4@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user5@event.com', "Recipient email is not correct"
 		assert_equal "A new item has been posted: " + @item1.name + " in event " + @item1.event.name, email.subject, "Email subject is incorrect"
 		email_text = read_fixture('item_created_message').join.sub!("__ITEM_LINK__", item_url(@item1, host: "localhost:3000"))
 		assert_equal email_text, email.text_part.body.to_s, "Email body is incorrect"
@@ -32,12 +32,12 @@ class EventMailerTest < ActionMailer::TestCase
 		email = EventMailer.item_modified_message(@item1).deliver_now!
 		assert_not ActionMailer::Base.deliveries.empty?
 		assert_equal ['info@comemalaka.com'], email.from, "Sender email is not correct"
-		assert_includes email.to, 'Lasse Lund', "Recipient email is not correct"
-		assert_includes email.to, 'Elyasin Shaladi', "Recipient email is not correct"
-		assert_includes email.to, 'Theo Goumas', "Recipient email is not correct"
-		assert_includes email.to, 'Neal Mundy', "Recipient email is not correct"
-		assert_includes email.to, 'Juan Cabrera', "Recipient email is not correct"
-		assert_includes email.to, 'Nuno Fonseca', "Recipient email is not correct"
+		assert_includes email.to, 'organizer@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user1@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user2@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user3@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user4@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user5@event.com', "Recipient email is not correct"
 		assert_equal "Item " + @item1.name + " in event " + @item1.event.name + " was modified", email.subject, "Email subject is incorrect"
 		email_text = read_fixture('item_modified_message').join.sub!("__ITEM_LINK__", item_url(@item1, host: "localhost:3000"))
 		assert_equal email_text, email.text_part.body.to_s, "Email body is incorrect"
@@ -47,12 +47,12 @@ class EventMailerTest < ActionMailer::TestCase
 		email = EventMailer.item_deleted_message(@item1).deliver_now!
 		assert_not ActionMailer::Base.deliveries.empty?
 		assert_equal ['info@comemalaka.com'], email.from, "Sender email is not correct"
-		assert_includes email.to, 'Lasse Lund', "Recipient email is not correct"
-		assert_includes email.to, 'Elyasin Shaladi', "Recipient email is not correct"
-		assert_includes email.to, 'Theo Goumas', "Recipient email is not correct"
-		assert_includes email.to, 'Neal Mundy', "Recipient email is not correct"
-		assert_includes email.to, 'Juan Cabrera', "Recipient email is not correct"
-		assert_includes email.to, 'Nuno Fonseca', "Recipient email is not correct"
+		assert_includes email.to, 'organizer@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user1@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user2@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user3@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user4@event.com', "Recipient email is not correct"
+		assert_includes email.to, 'user5@event.com', "Recipient email is not correct"
 		assert_equal "Item " + @item1.name + " in event " + @item1.event.name + " was deleted", email.subject, "Email subject is incorrect"
 		assert_equal read_fixture('item_deleted_message').join, email.text_part.body.to_s, "Email body is incorrect"
 	end
