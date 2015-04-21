7eeb9c0e85367f406c18ff1fdafb0163770e5a80
Elyasin Shaladi <Elyas.Shaladi@gmail.com>
2015-04-21 22:55:18 +0800

    View test for home controller

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,6 +6,13 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success, "Respnse must be success"
     assert_template :index
+    # Test the view
+    assert_select "title", "Welcome to Come Malaka"
+    assert_select "h2", "Packoooook Malaka"
+    assert_select "p", 'Welcome to Come Malaka. Sign in or sign up for an account to start using the Come Malaka app.'
+    assert_select "p a", 2
+    assert_select "a:match('href', ?)", new_user_session_path
+    assert_select "a:match('href', ?)", new_user_registration_path
   end
 
   test "authenticated user must be redirected to event's page" do
